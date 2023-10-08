76ef54f52669e61ccf7a0b0089e5dc8decf241c0
eisec <info@eisec.ru>
2023-10-08 23:06:10 +0300

    -add tailwinds

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
         tg.close()
     }
     return (
-        <div className="App">
+        <div className={`container mx-auto px-6`}>
             <h1>Work</h1>
-            <button onClick={onClose}>
+            <button className={'rounded-md bg-blue-600 text-white px-4 py-2 leading-[1]'} onClick={onClose}>
                 Закрыть
             </button>
         </div>
